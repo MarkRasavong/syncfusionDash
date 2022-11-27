fe24a74e24ca269ac91777f1be03df8984feb4df
Mark Rasavong <rasavong.mark@gmail.com>
2022-11-27 12:14:00 +0100

    add: Routing skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { FiSettings } from 'react-icons/fi'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
@@ -40,6 +40,32 @@ const App = () => {
 						<div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
 							navbar
 						</div>
+					</div>
+					{/* Routing */}
+					<div>
+						<Routes>
+							{/* Dashboard */}
+							<Route path="/" element="ecommerce" />
+							<Route path="/ecommerce" element="ecommerce" />
+							{/* Pages */}
+							<Route path="/orders" element="orders" />
+							<Route path="/employees" element="employees" />
+							<Route path="/customers" element="customers" />
+							{/* Apps */}
+							<Route path="/kanban" element="kanban" />
+							<Route path="/editor" element="editor" />
+							<Route path="/calendar" element="calendar" />
+							<Route path="/color-picker" element="colorPicker" />
+							{/* Charts */}
+							<Route path="/line" element="line" />
+							<Route path="/area" element="area" />
+							<Route path="/bar" element="bar" />
+							<Route path="/pie" element="pie" />
+							<Route path="/financial" element="financial" />
+							<Route path="/color-mapping" element="colorMapping" />
+							<Route path="/pyramid" element="pyramid" />
+							<Route path="/stacked" element="stacked" />
+						</Routes>
 					</div>
 				</div>
 			</BrowserRouter>
