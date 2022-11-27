d50f646e0b10e84516e86ffa401329bf5dc17081
Mark Rasavong <rasavong.mark@gmail.com>
2022-11-27 12:02:22 +0100

    add: Navbar  skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ const App = () => {
 		<div>
 			<BrowserRouter>
 				<div className="flex relative dark:bg-main-dark-bg">
+					{/* floating settings buutton */}
 					<div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
 						<TooltipComponent content="Settings">
 							<button
@@ -22,6 +23,7 @@ const App = () => {
 							</button>
 						</TooltipComponent>
 					</div>
+					{/* dynamic sidebar */}
 					{activeMenu ? (
 						<div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
 							Sidebar
@@ -29,6 +31,16 @@ const App = () => {
 					) : (
 						<div className="w-0 dark:bg-secondary-dark-bg">Sidebar w-0</div>
 					)}
+					{/* navbar */}
+					<div
+						className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+							activeMenu ? 'md:ml-72' : 'flex-2'
+						}`}
+					>
+						<div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+							navbar
+						</div>
+					</div>
 				</div>
 			</BrowserRouter>
 		</div>
