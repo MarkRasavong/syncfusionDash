0b00c37509882d7b0ba96536fffbbb8503b3fa41
Mark Rasavong <rasavong.mark@gmail.com>
2022-12-21 15:42:27 +0100

    add: Sparkline component

--- src/components/Charts/SparkLine.tsx
+++ src/components/Charts/SparkLine.tsx
@@ -1,5 +1,51 @@
 import React from 'react'
+import {
+	SparklineComponent,
+	Inject,
+	SparklineTooltip,
+} from '@syncfusion/ej2-react-charts'
 
-const SparkLine = () => <div>SparkLine</div>
+interface SparklineProps {
+	id: string
+	height: string
+	width: string
+	color: string
+	type: 'Line' | 'Column' | 'WinLoss' | 'Pie' | 'Area' | undefined
+	currentColor: string
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	data: Object[] | undefined
+}
+
+const SparkLine = ({
+	id,
+	width,
+	height,
+	color,
+	type,
+	currentColor,
+	data,
+}: SparklineProps) => (
+	<SparklineComponent
+		id={id}
+		height={height}
+		width={width}
+		lineWidth={1}
+		valueType="Numeric"
+		fill={color}
+		border={{ color: currentColor, width: 2 }}
+		dataSource={data}
+		xName="x"
+		yName="y"
+		type={type}
+		tooltipSettings={{
+			visible: true,
+			// eslint-disable-next-line no-template-curly-in-string
+			format: '${x}: data ${y}',
+			trackLineSettings: { visible: true },
+		}}
+	>
+		<Inject services={[SparklineTooltip]} />
+	</SparklineComponent>
+)
 
 export default SparkLine
