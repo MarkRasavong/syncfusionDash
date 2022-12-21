d0ec614fbd779585b417569e19614adc887055f1
Mark Rasavong <rasavong.mark@gmail.com>
2022-12-21 20:19:40 +0100

    add: add and display orders table with full functionality

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,9 +1,59 @@
 import React from 'react'
+import {
+	GridComponent,
+	ColumnsDirective,
+	ColumnDirective,
+	Resize,
+	Sort,
+	ContextMenu,
+	Filter,
+	Page,
+	ExcelExport,
+	PdfExport,
+	Edit,
+	Inject,
+	TextAlign,
+} from '@syncfusion/ej2-react-grids'
+
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy'
 import { Header } from '../components'
 
 const Orders = () => (
-	<div>
+	<div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
 		<Header title="Orders" category="Page" />
+		<GridComponent
+			id="gridcomp"
+			dataSource={ordersData}
+			allowPaging
+			allowSorting
+		>
+			<ColumnsDirective>
+				{ordersGrid.map((ele, idx) => (
+					<ColumnDirective
+						key={`ColDirect_${idx + Math.random()}`}
+						headerText={ele.headerText}
+						template={ele.template}
+						textAlign={(ele.textAlign as TextAlign) || undefined}
+						width={ele.width}
+						field={ele.field}
+						editType={ele.editType}
+						format={ele.format}
+					/>
+				))}
+			</ColumnsDirective>
+			<Inject
+				services={[
+					Resize,
+					Sort,
+					ContextMenu,
+					Filter,
+					Page,
+					ExcelExport,
+					Edit,
+					PdfExport,
+				]}
+			/>
+		</GridComponent>
 	</div>
 )
 
