f430494e9c86dcd834edfbf85b5997ffe58aa637
Mark Rasavong <rasavong.mark@gmail.com>
2022-12-21 12:41:46 +0100

    add: resuable Button component for homepage

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,21 @@
 import React from 'react'
 
-const Button = () => <div>Button</div>
+interface ButtonProps {
+	bgColor: string
+	color: string
+	size: string
+	text: string
+	borderRadius: string
+}
+
+const Button = ({ bgColor, color, size, text, borderRadius }: ButtonProps) => (
+	<button
+		type="button"
+		className={`text-${size} p-3 hover:drop-shadow-xl`}
+		style={{ backgroundColor: bgColor, color, borderRadius }}
+	>
+		{text}
+	</button>
+)
 
 export default Button
