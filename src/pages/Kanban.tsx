81545a0333fcf625139361dac899dc8bff6160e6
Mark Rasavong <rasavong.mark@gmail.com>
2022-12-22 18:24:01 +0100

    add: render Kanband page

--- src/pages/Kanban.tsx
+++ src/pages/Kanban.tsx
@@ -1,5 +1,35 @@
 import React from 'react'
+import {
+	KanbanComponent,
+	ColumnsDirective,
+	ColumnDirective,
+} from '@syncfusion/ej2-react-kanban'
 
-const Kanban = () => <div>Kanban</div>
+import { kanbanData, kanbanGrid } from '../data/dummy'
+import { Header } from '../components'
+
+const Kanban = () => (
+	<div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+		<Header category="App" title="Kanban" />
+		<KanbanComponent
+			id="kanban"
+			keyField="Status"
+			dataSource={kanbanData}
+			cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
+		>
+			<ColumnsDirective>
+				{kanbanGrid.map((ele) => (
+					<ColumnDirective
+						key={`KanbanColDirective_${Math.random()}`}
+						headerText={ele.headerText}
+						keyField={ele.keyField}
+						isExpanded={ele.isExpanded}
+						allowToggle={ele.allowToggle}
+					/>
+				))}
+			</ColumnsDirective>
+		</KanbanComponent>
+	</div>
+)
 
 export default Kanban
