e653b3099de294bbdf8c0da5f5f2e1b733cd06c5
Mark Rasavong <rasavong.mark@gmail.com>
2022-12-25 11:36:05 +0100

    fix: tooltip settings recieve theme color

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ import {
 } from './contexts/ContextProvider'
 
 const App = () => {
-	const { activeMenu, themeSettings, setThemeSettings } =
+	const { activeMenu, themeSettings, setThemeSettings, currentColor } =
 		useStateContext() as StoreContextInterface
 
 	return (
@@ -44,7 +44,7 @@ const App = () => {
 							<button
 								type="button"
 								className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-								style={{ background: 'blue', borderRadius: '50%' }}
+								style={{ background: currentColor, borderRadius: '50%' }}
 							>
 								<FiSettings />
 							</button>
