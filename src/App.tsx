22dad99d165b80ad6a13a79d231f952d0d56e1c9
Mark Rasavong <rasavong.mark@gmail.com>
2022-12-26 12:40:16 +0100

    style: Refine ecomm dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -40,24 +40,20 @@ const App = () => {
 		<div className={currentMode === 'Dark' ? 'dark' : ''}>
 			<BrowserRouter>
 				<div className="flex relative dark:bg-main-dark-bg">
-					{/* floating settings buutton */}
 					<div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
-						<TooltipComponent
-							content="Settings"
-							onClick={() => setThemeSettings(true)}
-						>
+						<TooltipComponent content="Settings">
 							<button
 								type="button"
-								className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+								onClick={() => setThemeSettings(true)}
 								style={{ background: currentColor, borderRadius: '50%' }}
+								className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
 							>
 								<FiSettings />
 							</button>
 						</TooltipComponent>
 					</div>
-					{/* dynamic sidebar */}
 					{activeMenu ? (
-						<div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+						<div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
 							<Sidebar />
 						</div>
 					) : (
@@ -65,7 +61,6 @@ const App = () => {
 							<Sidebar />
 						</div>
 					)}
-					{/* navbar */}
 					<div
 						className={
 							activeMenu
@@ -73,11 +68,9 @@ const App = () => {
 								: 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
 						}
 					>
-						<div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+						<div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
 							<Navbar />
 						</div>
-
-						{/* Routing */}
 						<div>
 							{themeSettings && <ThemeSettings />}
 							<Routes>
