4338bbbeb726bb1f42af00a00db90ec7cf28f7c0
Mark Rasavong <rasavong.mark@gmail.com>
2022-12-26 11:45:54 +0100

    style: Button component with optional props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,16 +6,34 @@ interface ButtonProps {
 	size: string
 	text: string
 	borderRadius: string
+	icon?: JSX.Element
+	bgHoverColor?: string
+	width?: string
 }
 
-const Button = ({ bgColor, color, size, text, borderRadius }: ButtonProps) => (
+const Button = ({
+	bgColor,
+	color,
+	size,
+	text,
+	borderRadius,
+	bgHoverColor,
+	icon,
+	width,
+}: ButtonProps) => (
 	<button
 		type="button"
-		className={`$text-${size} p-3 hover:drop-shadow-xl`}
+		className={`text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
 		style={{ backgroundColor: bgColor, color, borderRadius }}
 	>
-		{text}
+		{icon} {text}
 	</button>
 )
+
+Button.defaultProps = {
+	icon: '',
+	bgHoverColor: 'inherit',
+	width: 'auto',
+}
 
 export default Button
